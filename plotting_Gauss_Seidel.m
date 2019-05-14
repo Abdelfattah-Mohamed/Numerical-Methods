5db291e84a73977c3f163badad0100897b5f647f
Gamal Eldeen Ahmed Khalaf <superzoro23@yahoo.com>
2019-05-15 01:29:28 +0200

    guass siedle plot

---
+++ plotting_Gauss_Seidel.m
@@ -0,0 +1,15 @@
+function plotting_Gauss_Seidel(x)
+
+    figure('name','plotting_Gauss_Seidel');
+    [mm,nn] = size(x);
+    for m = 1: mm
+        y = [];
+        for t = 1:nn
+            y = [y, x(m,t)];                        
+        end
+        plot(1:1:nn, y, 'k');
+        hold on;
+    end
+    xlabel('Iteration');
+    ylabel('Roots');
+end
